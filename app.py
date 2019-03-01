6910b71d2b53506a73fa14764e485238f18778d8
myke <MikulasZelinka@users.noreply.github.com>
2019-03-01 10:42:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
+import requests
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+SLACK_HOOK_URL = "https://hooks.slack.com/services/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"
 
 @app.route('/')
 def index():
-    """Return homepage."""
-    json_data =
-    {
-        "text": "https://media1.giphy.com/media/2MmETUpDuWgCs/giphy.gif"
-    }
-    return jsonify(json_data)
+    requests.post(SLACK_HOOK_URL,
+                             json={
+                               'text': 'https://media1.giphy.com/media/2MmETUpDuWgCs/giphy.gif'
+                             })    
 
 
 if __name__ == '__main__':
