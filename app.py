7d9809d7558caaba43d528cea0c20e8a51eb0d3c
myke <MikulasZelinka@users.noreply.github.com>
2019-03-01 10:47:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ SLACK_HOOK_URL = "https://hooks.slack.com/services/T0J14PD7E/BGKM25L5N/gBDrs5RpR
 
 @app.route('/')
 def index():
-    requests.post(SLACK_HOOK_URL,
-                             json={
-                               'text': 'https://media1.giphy.com/media/2MmETUpDuWgCs/giphy.gif'
-                             })    
+    requests.post(SLACK_HOOK_URL, json={'text': 'https://media1.giphy.com/media/2MmETUpDuWgCs/giphy.gif'})
+    json_data = {'Hello': 'World!'}
+    return jsonify(json_data)
 
 
 if __name__ == '__main__':
