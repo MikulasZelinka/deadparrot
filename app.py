32f37747dcd8534bc637d60b5469abee98bdf189
myke <MikulasZelinka@users.noreply.github.com>
2019-03-01 10:19:53 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Return homepage."""
+    json_data = {'Hello': 'World!'}
+    return jsonify(json_data)
+
+
+if __name__ == '__main__':
+    app.run()
