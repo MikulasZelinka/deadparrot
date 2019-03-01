605e87d71bb7437e0680d0087fab8d05619c1a2d
myke <MikulasZelinka@users.noreply.github.com>
2019-03-01 11:15:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
-
-SLACK_HOOK_URL = "https://hooks.slack.com/services/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"
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/')
 def index():
-    requests.post(SLACK_HOOK_URL, json={'text': ''})
     return jsonify(
         response_type='in_channel',
         text='https://media1.giphy.com/media/2MmETUpDuWgCs/giphy.gif',
