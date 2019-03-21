770ee6029e129d540260318be2fe154fd0a1ec3d
Tomáš Trnka <tomas@trnkatomas.eu>
2019-03-21 09:58:43 +0100

    Update app.py
    added gif for dealing better with the real wold

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
 GIFZ = {
+    'aperol': 'https://media2.giphy.com/media/dSY9GgU0fWS8o/giphy.gif',
     'parrot': 'https://media1.giphy.com/media/2MmETUpDuWgCs/giphy.gif',
     'spam': 'https://media1.giphy.com/media/RI4LTRjrVJhTskGtrb/giphy.gif',
     'wound': 'https://media3.giphy.com/media/Tim0q7zolF3fa/giphy.gif',
