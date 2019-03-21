e43bed3c7807f76c154c95fc44d7c8be43a72926
myke <MikulasZelinka@users.noreply.github.com>
2019-03-21 10:18:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ GIFZ = {
     'boo': 'https://media2.giphy.com/media/HFHTld1DUaLlu/giphy.gif',
     'interview': 'https://media0.giphy.com/media/wi9yHmX7Sztuw/giphy.gif',
     'inquisition': 'https://media0.giphy.com/media/y5W98cY6OCudO/giphy.gif',
-    'grannies': 'https://media2.giphy.com/media/DdRljUx9yiZP2/giphy.gif'
+    'grannies': 'https://media2.giphy.com/media/DdRljUx9yiZP2/giphy.gif',
+    'oui': 'https://i.pinimg.com/originals/af/98/59/af985985493d2763b248fa01001ecd78.gif'
 }
 
 @app.route('/', methods=['GET', 'POST'])
